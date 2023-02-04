af4766337a043ac927a7e6ed7cff3077c7f01901
HA THANH DUNG <hathanhdung1995@gmail.com>
2023-02-05 06:51:50 +0700

    Commit

---
+++ MyPhamTrueLife/MyPhamTrueLife.Web/Controllers/Admin/WeatherForecastController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyPhamTrueLife.BLL.Interface;
+using MyPhamTrueLife.DAL.Models;
+using MyPhamTrueLife.Web.Models.Response;
+using System;
+using System.Threading.Tasks;
+
+namespace MyPhamTrueLife.Web.Controllers.Admin
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly IInfoCapicityService _infoCapicityService;
+        public WeatherForecastController(IInfoCapicityService infoCapicityService)
+        {
+            _infoCapicityService = infoCapicityService;
+        }
+
+        [Route("create")]
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<ResponseResult<string>> Post([FromBody] InfoCapacity value, int userId)
+        {
+            try
+            {
+                //int currentUserId = GetCurrentUserId();
+                var result = await _infoCapicityService.InsertCapacityAsync(value, userId);
+                return new ResponseResult<string>(RetCodeEnum.Ok, RetCodeEnum.Ok.ToString(), result.ToString());
+            }
+            catch (Exception ex)
+            {
+                return new ResponseResult<string>(RetCodeEnum.ApiError, ex.Message, null);
+            }
+        }
+
+
+    }
+}
