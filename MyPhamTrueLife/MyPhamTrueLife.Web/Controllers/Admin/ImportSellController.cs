3bf2f80ce618c926f6521b10db423457d32839da
Phạm Hữu Tính <92001851+tinhph1108@users.noreply.github.com>
2023-02-09 22:38:54 +0700

    commit

--- MyPhamTrueLife/MyPhamTrueLife.Web/Controllers/Admin/ImportSellController.cs
+++ MyPhamTrueLife/MyPhamTrueLife.Web/Controllers/Admin/ImportSellController.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MyPhamTrueLife.BLL.Interface;
+using MyPhamTrueLife.DAL.Models1;
+using MyPhamTrueLife.DAL.Models.Utils;
+using MyPhamTrueLife.Web.Base;
+using MyPhamTrueLife.Web.Models.Response;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +15,63 @@ namespace MyPhamTrueLife.Web.Controllers.Admin
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ImportSellController : ControllerBase
+    [Authorize]
+    public class ImportSellController : BaseApiController
     {
+        private readonly IImportSellService _importSellService;
+        public ImportSellController(IImportSellService importSellService)
+        {
+            _importSellService = importSellService;
+        }
+
+        [Route("create")]
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<ResponseResult<string>> Post([FromBody] InfoImportSell value, int userId)
+        {
+            try
+            {
+                //int currentUserId = GetCurrentUserId();
+                var result = await _importSellService.CreateImportSellServiceAsync(value, userId);
+                return new ResponseResult<string>(RetCodeEnum.Ok, RetCodeEnum.Ok.ToString(), result.ToString());
+            }
+            catch (Exception ex)
+            {
+                return new ResponseResult<string>(RetCodeEnum.ApiError, ex.Message, null);
+            }
+        }
+
+        [Route("delete")]
+        [HttpDelete]
+        [AllowAnonymous]
+        public async Task<ResponseResult<string>> Delete(int importId, int userId)
+        {
+            try
+            {
+                //int currentUserId = GetCurrentUserId();
+                var result = await _importSellService.DeleteImportSellServiceAsync(importId, userId);
+                return new ResponseResult<string>(RetCodeEnum.Ok, RetCodeEnum.Ok.ToString(), result.ToString());
+            }
+            catch (Exception ex)
+            {
+                return new ResponseResult<string>(RetCodeEnum.ApiError, ex.Message, null);
+            }
+        }
+
+        [HttpPost]
+        [Route("list")]
+        [AllowAnonymous]
+        public async Task<ResponseResult<ResponseList>> list(int page = 1, int limit = 25)
+        {
+            try
+            {
+                var result = await _importSellService.ListImportSellServiceAsync(page, limit);
+                return new ResponseResult<ResponseList>(RetCodeEnum.Ok, "Danh sách nhập hàng", result);
+            }
+            catch (Exception ex)
+            {
+                return new ResponseResult<ResponseList>(RetCodeEnum.ApiError, ex.Message, null);
+            }
+        }
     }
 }
