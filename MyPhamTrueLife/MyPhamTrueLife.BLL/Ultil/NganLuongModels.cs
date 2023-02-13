4fd046018bae77057b7a17c8f1ff400d6ff4bbbc
Phan Trinh <trinhp2t@gmail.com>
2023-02-13 09:27:02 +0700

    commit

---
+++ MyPhamTrueLife/MyPhamTrueLife.BLL/Ultil/NganLuongModels.cs
@@ -0,0 +1,363 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyPhamTrueLife.BLL.Ultil
+{
+    public class NganLuongModels
+    {
+        //Thông tin đầu vào của hàm yêu cầu rút tiền SetCashoutRequest
+        public class WithdrawMoneyRequest
+        {
+            private String _Funtion = "SetCashoutRequest";
+
+            public String Funtion
+            {
+                get { return _Funtion; }
+            }
+            private String _Merchant_id = String.Empty;
+            public String Merchant_id
+            {
+                get { return _Merchant_id; }
+                set { _Merchant_id = value; }
+            }
+            private String _Receiver_email = String.Empty;
+
+            public String Receiver_email
+            {
+                get { return _Receiver_email; }
+                set { _Receiver_email = value; }
+            }
+            private String _Merchant_password = String.Empty;
+
+            public String Merchant_password
+            {
+                get { return _Merchant_password; }
+                set { _Merchant_password = value; }
+            }
+            private String _Ref_code = String.Empty;
+
+            public String RefCode
+            {
+                get { return _Ref_code; }
+                set { _Ref_code = value; }
+            }
+            private long _Total_amount = 0;
+            public long Total_amount
+            {
+                get { return _Total_amount; }
+                set { _Total_amount = value; }
+            }
+            private int _Account_type = 0;
+            public int AccountType
+            {
+                get { return _Account_type; }
+                set { _Account_type = value; }
+            }
+            private String _bank_code = String.Empty;
+
+            public String bank_code
+            {
+                get { return _bank_code; }
+                set { _bank_code = value; }
+            }
+            private String _Card_fullname = String.Empty;
+            public String Card_fullname
+            {
+                get { return _Card_fullname; }
+                set { _Card_fullname = value; }
+            }
+            private String _Card_number = String.Empty;
+            public String CardNumber
+            {
+                get { return _Card_number; }
+                set { _Card_number = value; }
+            }
+            private String _Card_month = String.Empty;
+            public String CardMonth
+            {
+                get { return _Card_month; }
+                set { _Card_month = value; }
+            }
+            private String _Card_year = String.Empty;
+            public String CardYear
+            {
+                get { return _Card_year; }
+                set { _Card_year = value; }
+            }
+            private String _branch_name = String.Empty;
+            public String BranchName
+            {
+                get { return _branch_name; }
+                set { _branch_name = value; }
+            }
+            private int? _zone_id = null;
+            public int? ZoneId
+            {
+                get { return _zone_id; }
+                set { _zone_id = value; }
+            }
+            private String _reason = String.Empty;
+            public String Reason
+            {
+                get { return _reason; }
+                set { _reason = value; }
+            }
+        }
+        //Thông tin trả về cảu hàm yêu cầu rút tiền SetCashoutRequest
+        public class WithdrawMoneyResponse
+        {
+            private String _error_code = string.Empty;
+
+            public String Error_code
+            {
+                get { return _error_code; }
+                set { _error_code = value; }
+            }
+            private String _Ref_code = String.Empty;
+
+            public String Ref_Code
+            {
+                get { return _Ref_code; }
+                set { _Ref_code = value; }
+            }
+            private String _transaction_id = String.Empty;
+
+            public String Transaction_Id
+            {
+                get { return _transaction_id; }
+                set { _transaction_id = value; }
+            }
+
+            private String _transaction_status = String.Empty;
+
+            public String Transaction_Status
+            {
+                get { return _transaction_status; }
+                set { _transaction_status = value; }
+            }
+
+            private int? _total_amount = null;
+
+            public int? Total_Amount
+            {
+                get { return _total_amount; }
+                set { _total_amount = value; }
+            }
+        }
+        //Thông tin đầu vào của hàm kiểm tra trạng thái giao dịch rút tiền(hàm cũ)
+        public class CheckWithdrawalStatusRequestOld
+        {
+            private String _Funtion = "CheckCashout";
+
+            public String Funtion
+            {
+                get { return _Funtion; }
+            }
+            private String _Merchant_id = String.Empty;
+            public String Merchant_id
+            {
+                get { return _Merchant_id; }
+                set { _Merchant_id = value; }
+            }
+            private String _Receiver_email = String.Empty;
+
+            public String Receiver_email
+            {
+                get { return _Receiver_email; }
+                set { _Receiver_email = value; }
+            }
+            private String _Merchant_password = String.Empty;
+
+            public String Merchant_password
+            {
+                get { return _Merchant_password; }
+                set { _Merchant_password = value; }
+            }
+            private String _Ref_code = String.Empty;
+
+            public String RefCode
+            {
+                get { return _Ref_code; }
+                set { _Ref_code = value; }
+            }
+            private String _transaction_id = String.Empty;
+
+            public String TransactionId
+            {
+                get { return _transaction_id; }
+                set { _transaction_id = value; }
+            }
+        }
+        //Thông tin đầu vào của hàm kiểm tra trạng thái giao dịch rút tiền(hàm mới)
+        public class CheckWithdrawalStatusRequestNew
+        {
+            private String _Funtion = "CheckCashout";
+
+            public String Funtion
+            {
+                get { return _Funtion; }
+            }
+            private String _Merchant_id = String.Empty;
+            public String Merchant_id
+            {
+                get { return _Merchant_id; }
+                set { _Merchant_id = value; }
+            }
+            private String _Receiver_email = String.Empty;
+
+            public String Receiver_email
+            {
+                get { return _Receiver_email; }
+                set { _Receiver_email = value; }
+            }
+            private String _Merchant_password = String.Empty;
+
+            public String Merchant_password
+            {
+                get { return _Merchant_password; }
+                set { _Merchant_password = value; }
+            }
+            private String _Ref_code = String.Empty;
+
+            public String RefCode
+            {
+                get { return _Ref_code; }
+                set { _Ref_code = value; }
+            }
+            private String _transaction_id = String.Empty;
+
+            public String TransactionId
+            {
+                get { return _transaction_id; }
+                set { _transaction_id = value; }
+            }
+        }
+        //Thông tin quả trả về của hàm kiểm tra trạng thái giao dịch rút tiền
+        public class CheckWithdrawalStatusResponse
+        {
+            private String _error_code = string.Empty;
+            public String Error_code
+            {
+                get { return _error_code; }
+                set { _error_code = value; }
+            }
+            private String _Ref_code = String.Empty;
+            public String Ref_Code
+            {
+                get { return _Ref_code; }
+                set { _Ref_code = value; }
+            }
+            private int? _total_amount = null;
+            public int? Total_Amount
+            {
+                get { return _total_amount; }
+                set { _total_amount = value; }
+            }
+            private int? _transaction_status = null;
+            public int? Transaction_Status
+            {
+                get { return _transaction_status; }
+                set { _transaction_status = value; }
+            }
+            private int? _transaction_id = null;
+            public int? Transaction_Id
+            {
+                get { return _transaction_id; }
+                set { _transaction_id = value; }
+            }
+            private String _transaction_refund_id = String.Empty;
+            public String Transaction_Refund_Id
+            {
+                get { return _transaction_refund_id; }
+                set { _transaction_refund_id = value; }
+            }
+        }
+        public class WithdrawalMoney
+        {
+            public string RefCode { get; set; }
+            public long TotalAmount { get; set; }
+            public string BankCode { get; set; }
+            public string CardFullName { get; set; }
+            public string CardNumber { get; set; }
+            public string CardMonth { get; set; }
+            public string CardYear { get; set; }
+            public string BranchName { get; set; }
+            public int? ZoneId { get; set; }
+            public string Reason { get; set; }
+        }
+        public class CheckWithdrawalStatus
+        {
+            public string TransactionId { get; set; }
+            public string RefCode { get; set; }
+        }
+        //Tham số của thanh toán
+        public class PaymentRequest
+        {
+            public int checkoutType { get; set; } = 4;
+            //public string paymentMethod { get; set; } = "ATM_ON";
+            public string tokenKey { get; set; }
+            public bool allowDomestic { get; set; } = true;
+            public string orderCode { get; set; }
+            public string customMerchantId { get; set; }
+            public double amount { get; set; }
+            public string currency { get; set; }
+            public string orderDescription { get; set; }
+            public int totalItem { get; set; }
+            public string returnUrl { get; set; } //Đưa đường dẫn link cần đá về app chổ này
+            public string cancelUrl { get; set; }
+            public string buyerName { get; set; }
+            public string buyerEmail { get; set; }
+            public string buyerPhone { get; set; }
+            public string buyerAddress { get; set; }
+            public string buyerCity { get; set; }
+            public string buyerCountry { get; set; }
+            public string signature { get; set; }
+        }
+        public class PaymentResponse
+        {
+            public string code { get; set; }
+            public string message { get; set; }
+            public string checkoutUrl { get; set; }
+            public string transactionCode { get; set; }
+            public string signature { get; set; }
+        }
+        //Tham số của kiểm tra thanh toán
+        public class CheckPaymentRequest
+        {
+            public string tokenKey { get; set; }
+            public string transactionCode { get; set; }
+            public string signature { get; set; }
+        }
+        public class CheckPaymentResponse
+        {
+            public string code { get; set; }
+            public string message { get; set; }
+            public string signature { get; set; }
+            public string transactionCode { get; set; }
+            public string orderCode { get; set; }
+            public string amount { get; set; }
+            public string currency { get; set; }
+            public string buyerEmail { get; set; }
+            public string buyerPhone { get; set; }
+            public string cardNumber { get; set; }
+            public string buyerName { get; set; }
+            public string status { get; set; }
+            public string reason { get; set; }
+            public string description { get; set; }
+            public bool installment { get; set; }
+            public bool is3D { get; set; }
+            public int month { get; set; }
+            public string bankCode { get; set; }
+            public string bankName { get; set; }
+            public string method { get; set; }
+            public string bankType { get; set; }
+            public long transactionTime { get; set; }
+            public long successTime { get; set; }
+            public string bankHotline { get; set; }
+            public double merchantFee { get; set; }
+            public double payerFee { get; set; }
+            public string authenCode { get; set; }
+        }
+    }
+}
---
+++ MyPhamTrueLife/MyPhamTrueLife.Web/Models/NganLuong/APIThanhToan.cs
@@ -0,0 +1,223 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using static MyPhamTrueLife.BLL.Ultil.NganLuongModels;
+
+namespace MyPhamTrueLife.Web.Models.NganLuong
+{
+    public class APIThanhToan
+    {
+        public async Task<PaymentResponse> Payment(PaymentRequest value, string TokenKey, string CheckSum)
+        {
+            if (string.IsNullOrEmpty(TokenKey) || string.IsNullOrEmpty(CheckSum) || value == null)
+            {
+                return null;
+            }
+            value.tokenKey = TokenKey;
+            value.signature = CreateSignaturePayment(CheckSum, value);
+            var jsonData = JsonConvert.SerializeObject(value);
+            byte[] payload = Encoding.UTF8.GetBytes(jsonData);
+            #region Headers for Request
+            IDictionary<string, string> signedHeaders = new Dictionary<string, string>();
+            signedHeaders.Add("Content-Type", "application/json");
+            signedHeaders.Add("Accept", "*/*");
+            signedHeaders.Add("Accept-Encoding", "gzip, deflate, br");
+            signedHeaders.Add("Connection", "keep-alive");
+            signedHeaders.Add("Cookie", "PHPSESSID=dv86bnthu79t934jqvqp3rei42");
+            signedHeaders.Add("Cache-Control", "no-cache");
+            #endregion
+
+            #region Call Api
+            //onepayout/api/v1/customers should use in appsetting with configuration
+            var url = "https://alepay-v3-sandbox.nganluong.vn/api/v3/checkout/request-payment";
+
+            var response = await FundTransferPayment(value, url, signedHeaders);
+            return response ?? null;
+            #endregion
+        }
+        private async Task<PaymentResponse> FundTransferPayment(PaymentRequest value, string uri, IDictionary<string, string> signedHeaders)
+        {
+            var client = new RestClient(uri);
+            var request = new RestRequest(Method.Post.ToString());
+            request.AddJsonBody(value, contentType: "application/json");
+            request.AddHeaders(signedHeaders);
+            var response = await client.ExecuteAsync(request);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var resContent = response.Content;
+                var data = JsonConvert.DeserializeObject<PaymentResponse>(resContent);
+                return data;
+            }
+            else
+            {
+                var resErrContent = response.Content;
+                var errData = JsonConvert.DeserializeObject<PaymentResponse>(resErrContent);
+                return errData.message != "" ? errData : null;
+            }
+        }
+        //Kiểm tra thanh toán
+        public async Task<CheckPaymentResponse> CheckPayment(CheckPaymentRequest value, string TokenKey, string CheckSum)
+        {
+            if (string.IsNullOrEmpty(TokenKey) || string.IsNullOrEmpty(CheckSum) || value == null)
+            {
+                return null;
+            }
+            value.tokenKey = TokenKey;
+            value.signature = CreateSignatureCheckPayment(CheckSum, value);
+            var jsonData = JsonConvert.SerializeObject(value);
+            byte[] payload = Encoding.UTF8.GetBytes(jsonData);
+            #region Headers for Request
+            IDictionary<string, string> signedHeaders = new Dictionary<string, string>();
+            signedHeaders.Add("Content-Type", "application/json");
+            signedHeaders.Add("Accept", "*/*");
+            signedHeaders.Add("Accept-Encoding", "gzip, deflate, br");
+            signedHeaders.Add("Connection", "keep-alive");
+            signedHeaders.Add("Cookie", "PHPSESSID=dv86bnthu79t934jqvqp3rei42");
+            signedHeaders.Add("Cache-Control", "no-cache");
+            #endregion
+
+            #region Call Api
+            //onepayout/api/v1/customers should use in appsetting with configuration
+            var url = "https://alepay-v3-sandbox.nganluong.vn/api/v3/checkout/get-transaction-info";
+
+            var response = await FundTransferCheckPayment(value, url, signedHeaders);
+            return response ?? null;
+            #endregion
+        }
+        private async Task<CheckPaymentResponse> FundTransferCheckPayment(CheckPaymentRequest value, string uri, IDictionary<string, string> signedHeaders)
+        {
+            var client = new RestClient(uri);
+            var request = new RestRequest(Method.Post.ToString());
+            request.AddJsonBody(value, contentType: "application/json");
+            request.AddHeaders(signedHeaders);
+            var response = await client.ExecuteAsync(request);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var resContent = response.Content;
+                var data = JsonConvert.DeserializeObject<CheckPaymentResponse>(resContent);
+                return data;
+            }
+            else
+            {
+                var resErrContent = response.Content;
+                var errData = JsonConvert.DeserializeObject<CheckPaymentResponse>(resErrContent);
+                return errData.message != "" ? errData : null;
+            }
+        }
+        //Tạo Signature thanh toán
+        public string CreateSignaturePayment(string CheckSum, PaymentRequest request)
+        {
+            if (request == null || string.IsNullOrEmpty(CheckSum))
+            {
+                return string.Empty;
+            }
+            //Chổ này không thay đổi vị trí vì chuỗi đã được sắp xếp theo thứ tự alphabet.
+            string data = "amount=" + request.amount + "&buyerAddress=" + request.buyerAddress + "&buyerCity=" + request.buyerCity + "&buyerCountry=" + request.buyerCountry + "&buyerEmail=" + request.buyerEmail +
+                "&buyerName=" + request.buyerName + "&buyerPhone=" + request.buyerPhone + "&cancelUrl=" + request.cancelUrl + "&currency=" + request.currency + "&customMerchantId=" + request.customMerchantId +
+                "&orderCode=" + request.orderCode + "&orderDescription=" + request.orderDescription + "&returnUrl=" + request.returnUrl + "&tokenKey=" + request.tokenKey + "&totalItem=" + request.totalItem;
+            var key = hmacSha256(CheckSum, data);
+            return hex(key);
+        }
+        //Tạo Signature kiểm tra thanh toán
+        public string CreateSignatureCheckPayment(string CheckSum, CheckPaymentRequest request)
+        {
+            if (request == null || string.IsNullOrEmpty(CheckSum))
+            {
+                return string.Empty;
+            }
+            //Chổ này không thay đổi vị trí vì chuỗi đã được sắp xếp theo thứ tự alphabet.
+            string data = "tokenKey=" + request.tokenKey + "&transactionCode=" + request.transactionCode;
+            var key = hmacSha256(CheckSum, data);
+            return hex(key);
+        }
+        //Thuật toán HMAC
+        private static byte[] hmacSha256(string key, string data) { return hmacSha256(Encoding.UTF8.GetBytes(key), data); }
+        private static byte[] hmacSha256(byte[] key, string data)
+        {
+            var kha = KeyedHashAlgorithm.Create("HMACSHA256");
+            kha.Key = key;
+            return kha.ComputeHash(Encoding.UTF8.GetBytes(data));
+        }
+        private static byte[] sha256Hash(string data) { return sha256Hash(Encoding.UTF8.GetBytes(data)); }
+        private static HashAlgorithm SHA256Algorithm = HashAlgorithm.Create("SHA-256");
+        private static byte[] sha256Hash(byte[] data) { return SHA256Algorithm.ComputeHash(data); }
+        private static string hex(byte[] data)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in data) sb.Append(b.ToString("x2"));
+            return sb.ToString();
+        }
+    }
+
+    //Tham số của thanh toán
+    public class PaymentRequest
+    {
+        public string tokenKey { get; set; }
+        public string orderCode { get; set; }
+        public string customMerchantId { get; set; }
+        public double amount { get; set; }
+        public string currency { get; set; }
+        public string orderDescription { get; set; }
+        public int totalItem { get; set; }
+        public string returnUrl { get; set; } //Đưa đường dẫn link cần đá về app chổ này
+        public string cancelUrl { get; set; }
+        public string buyerName { get; set; }
+        public string buyerEmail { get; set; }
+        public string buyerPhone { get; set; }
+        public string buyerAddress { get; set; }
+        public string buyerCity { get; set; }
+        public string buyerCountry { get; set; }
+        public string signature { get; set; }
+    }
+    public class PaymentResponse
+    {
+        public string code { get; set; }
+        public string message { get; set; }
+        public string checkoutUrl { get; set; }
+        public string transactionCode { get; set; }
+        public string signature { get; set; }
+    }
+    //Tham số của kiểm tra thanh toán
+    //public class CheckPaymentRequest
+    //{
+    //    public string tokenKey { get; set; }
+    //    public string transactionCode { get; set; }
+    //    public string signature { get; set; }
+    //}
+    public class CheckPaymentResponse
+    {
+        public string code { get; set; }
+        public string message { get; set; }
+        public string signature { get; set; }
+        public string transactionCode { get; set; }
+        public string orderCode { get; set; }
+        public string amount { get; set; }
+        public string currency { get; set; }
+        public string buyerEmail { get; set; }
+        public string buyerPhone { get; set; }
+        public string cardNumber { get; set; }
+        public string buyerName { get; set; }
+        public string status { get; set; }
+        public string reason { get; set; }
+        public string description { get; set; }
+        public bool installment { get; set; }
+        public bool is3D { get; set; }
+        public int month { get; set; }
+        public string bankCode { get; set; }
+        public string bankName { get; set; }
+        public string method { get; set; }
+        public string bankType { get; set; }
+        public long transactionTime { get; set; }
+        public long successTime { get; set; }
+        public string bankHotline { get; set; }
+        public double merchantFee { get; set; }
+        public double payerFee { get; set; }
+        public string authenCode { get; set; }
+    }
+}
